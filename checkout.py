175d03a47123107c87eb2746dce94e65b92e1eeb
anidmaryumich <anidmary@umich.edu>
2026-09-22 23:17:16 -0400

    update checkout.py

--- checkout.py
+++ checkout.py
@@ -1,3 +1,4 @@
-def calculate_total(prices):
-    total = sum(prices)
+def calculate_total(prices, tax_rate=0.06):
+    subtotal = sum(prices)
+    total = subtotal * (1 + tax_rate)
     return total
